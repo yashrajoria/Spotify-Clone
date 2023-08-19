33a07358b9ce0d700f4cb5ac8110f21db82e09db
yashrajoria <yashrajoria@gmail.com>
2023-08-19 14:06:27 +0100

    Added modal provider for authentication

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import AuthModal from "@/components/AuthModal";
+import Modal from "@/components/Modal";
+import React, { useEffect, useState } from "react";
+
+// This component serves as a Modal Provider, responsible for managing modal-related functionality.
+const ModalProvider = () => {
+    // State to track whether the component is mounted or not.
+    const [isMounted, setIsMounted] = useState(false);
+
+    // useEffect hook to set 'isMounted' to true once the component is mounted.
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    // If the component is not mounted yet, return null.
+    // This prevents any rendering before the component is ready.
+    if (!isMounted) {
+        return null;
+    }
+
+    // If the component is mounted, render the modal content or components.
+    return (
+        <div>
+            <AuthModal />
+        </div>
+    );
+};
+
+export default ModalProvider;
