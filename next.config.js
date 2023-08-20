775787c739014f0c81872fbb9f9a380b9fc2fb2a
yashrajoria <yashrajoria@gmail.com>
2023-08-20 18:46:29 +0100

    added config to display images from supabase

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["ucdhxxmaxteygquxmjwn.supabase.co"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
