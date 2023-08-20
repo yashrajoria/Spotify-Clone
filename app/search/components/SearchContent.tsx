7133ff70042bedd36bdcb5964b677d3df54c1091
yashrajoria <yashrajoria@gmail.com>
2023-08-20 19:59:36 +0100

    minor css fix

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -16,7 +16,7 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
         )
     }
     return (
-        <div className='flex flex-col gap-y-2 px-6 px-6'>{songs.map((song) => (
+        <div className='flex flex-col gap-y-2 px-6'>{songs.map((song) => (
             <div key={song.id} className='flex items-center gap-x-4 w-full'><div className="flex-1">
                 <MediaItem
                     data={song}
