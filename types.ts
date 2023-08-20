3a38300c361b9fa89a000921866056ce06e5efec
yashrajoria <yashrajoria@gmail.com>
2023-08-20 18:48:21 +0100

    interface for songs

--- types.ts
+++ types.ts
@@ -1,6 +1,16 @@
 import { metadata } from "./app/layout";
 import Stripe from "stripe";
 
+export interface Song {
+  id: string;
+  user_id: string;
+  author: string;
+  title: string;
+  song_path: string;
+  image_path: string;
+  author_path: string;
+}
+
 export interface UserDetails {
   id: string;
   first_name: string;
