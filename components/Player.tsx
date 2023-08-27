ceb53ab943912aa6237ea4e845b63f16128917a3
yashrajoria <yashrajoria@gmail.com>
2023-08-27 02:33:33 +0100

    Removed console logs

--- components/Player.tsx
+++ components/Player.tsx
@@ -9,11 +9,8 @@ const Player = () => {
     const player = usePlayer();
 
     const { song } = useGetSongById(player.activeId);
-    // console.log(player)
-    // console.log(song)
 
     const songUrl = useLoadSongUrl(song!)
-    // console.log(songUrl)
     if (!song || !songUrl || !player.activeId) {
         return null;
     }
