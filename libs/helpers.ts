1720a8719cf7c80b69fc9dcc45f2418c2fadbc8d
yashrajoria <yashrajoria@gmail.com>
2023-08-27 02:07:35 +0100

    helpers for stripe

--- libs/helpers.ts
+++ libs/helpers.ts
@@ -2,13 +2,14 @@ import { Price } from "@/types";
 
 export const getURL = () => {
   let url =
-    process.env.NEXT_PUBLIC_SITE_URL ??
-    process.env.NEXT_PUBLIC_VERCEL_URL ??
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
     "http://localhost:3000";
-
-  url = url.includes("http ") ? url : `https://${url}`;
+  // Make sure to include `https://` when not localhost.
+  url = url.includes("http") ? url : `https://${url}`;
+  console.log(url);
+  // Make sure to including trailing `/`.
   url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
-  console.log({ url });
   return url;
 };
 
@@ -19,25 +20,26 @@ export const postData = async ({
   url: string;
   data?: { price: Price };
 }) => {
-  console.log("Post request", url, data);
+  console.log("posting,", url, data);
 
-  const response = await fetch(url, {
+  const res: Response = await fetch(url, {
     method: "POST",
     headers: new Headers({ "Content-Type": "application/json" }),
     credentials: "same-origin",
     body: JSON.stringify(data),
   });
-  console.log({ response });
-  if (!response.ok) {
-    console.log("Error in post request", data, url);
-    throw new Error(response.statusText);
+
+  if (!res.ok) {
+    console.log("Error in postData", { url, data, res });
+
+    throw Error(res.statusText);
   }
 
-  return response.json();
+  return res.json();
 };
 
 export const toDateTime = (secs: number) => {
-  var time = new Date("1970-01-01T00:30:00Z");
-  time.setSeconds(secs);
-  return time;
+  var t = new Date("1970-01-01T00:30:00Z"); // Unix epoch start.
+  t.setSeconds(secs);
+  return t;
 };
